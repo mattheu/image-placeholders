e51de92d47a97705139609521d95ed1fa2164f69
Matth_eu <matthew@matth.eu>
2015-07-01 10:50:52 +0200

    initial scaffold plugin

---
+++ image-placeholder.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Image-placeholder
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: image-placeholder
+Domain Path: /languages
+*/
+
